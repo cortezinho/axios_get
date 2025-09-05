b80cf71b778c3d59992809366b4bcf43ed8b9dbe
cortezinho <gabrielcortez2802@gmail.com>
2025-09-05 20:29:29 -0300

    inserção do objeto api e mockup

--- App.js
+++ App.js
@@ -8,6 +8,39 @@ export default App(){
   //users - variável que conterá a lista de usuários
   const[users, setUsers] = useState([])
 
-  
+  // definir a URl da API que será usada para buscar os usuários
+  const API = "http://trocar_por_IP_local:9000/users"
+
+  //função para buscar os usuários da API
+  async function fetchUsers(){
+    try{
+      const response = await api.get(API)
+      setUsers(response.data)
+    }catch(error){
+      console.log("Error GET:", error.message)
+    }
+  }
+
+  // useEffect - identificar o inicio do App e chama a função fetchUsers
+  useEffect(()=>{
+    fetchUsers()
+  },[])
+
+  const _render() =>{
+    const vet = []
+    // iteração no Array 'users' para mostrar os usuários por meio de componentes View
+    users.map((item, index) => {
+      vet.push(
+        <View key={index}>
+          <Text style={styles.item}>
+              ID:{item.id}
+              Nome:{item.name}
+              Email:{item.email}
+          </Text>
+        </View>
+      )
+    })
+    return vet
+  }
 
 }
