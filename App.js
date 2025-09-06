c9d04399336143038ee998f38b3e4763e4eacf03
cortezinho <gabrielcortez2802@gmail.com>
2025-09-05 21:09:54 -0300

    inserção do objeto api e mockup

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import {View, Text, ScrollView, StyleSheet, Button} from "react-native"
 import api from './src/devices/api'
 
 // declaração do componente principal 'App'
-export default App(){
+export default function App(){
 
   //users - variável que conterá a lista de usuários
   const[users, setUsers] = useState([])
 
   // definir a URl da API que será usada para buscar os usuários
-  const API = "http://trocar_por_IP_local:9000/users"
+  const API = "http://10.110.12.38:9000/users"
 
   //função para buscar os usuários da API
   async function fetchUsers(){
@@ -26,7 +26,7 @@ export default App(){
     fetchUsers()
   },[])
 
-  const _render() =>{
+  const _render = () =>{
     const vet = []
     // iteração no Array 'users' para mostrar os usuários por meio de componentes View
     users.map((item, index) => {
@@ -43,4 +43,24 @@ export default App(){
     return vet
   }
 
+  // retorno do componente 'App' - define a estrutura da interface
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        GET - Listar Usuários
+      </Text>
+      <Button title="recarrrgar Lista" onPress={fetchUsers}/>
+      <ScrollView>
+        {_render()}
+      </ScrollView>
+    </View>
+  )
+
 }
+
+// definição dos estilos
+const styles = StyleSheet.create({
+  container : {flex:1, padding:20, marginTop:40},
+  title : {fontSize:22, fontWeight: "bold", marginBottom: 10},
+  item : {fontSize:12, marginTop:10}
+})
